f1849b3c0a3fc54906aebcbe50dc5acef089edac
MrSyme <fuenmayork@uninorte.edu.co>
2022-06-07 00:02:37 -0500

    mejor carrito

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -21,8 +21,8 @@ export default function Basket(props) {
                 <div key={item.id} className="rowk ">
                     <div className='col-2k'>{item.name}</div>
                     <div className='col-2k'>
-                        <button onClick={() => onAdd(item)} className="buttonk addk ">+</button>
-                        <button onClick={() => onRemove(item)} className="buttonk removek ">-</button>
+                        <button onClick={() => onAdd(item)} className="h-10  text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center   mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ">+</button>
+                        <button onClick={() => onRemove(item)} className="h-10  text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center   mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 ">-</button>
                     </div>
                     <div className='col-2k text-rightk' >
                         {item.qty}x${item.price}
@@ -39,7 +39,7 @@ export default function Basket(props) {
                         </div>
                         <hr />
                         <div className='rowk '>
-                            <button className='buttonk' onClick={() => {
+                            <button className='h-10 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' onClick={() => {
                                     setCartItems([])
                                     enqueueSnackbar("Transacción exitosa", {
                                     variant: "success",
